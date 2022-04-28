245fc71a535a505c3ef247e30c6ca934ec055310
David Gallardo <david.ga@strivelabs.io>
2022-04-28 15:34:27 +0200

    controllers

--- src/controller/task.ts
+++ src/controller/task.ts
@@ -1,16 +1,43 @@
+import { StatusCodes } from "http-status-codes";
+import { NotFoundError, ServerError } from "src/core/errors";
 import { Controller, controllerWrapper } from "../core/controller";
+import TaskService from "../service/task";
 
-const getAllTasks: Controller = (req) => {};
+const getAllTasks: Controller = (req) => ({
+  code: StatusCodes.OK,
+  value: TaskService.getAllTasks(),
+});
 
-const createTask: Controller = (req) => {};
+const throwIfNotFound =
+  (controller: Controller): Controller =>
+  (req) => {
+    try {
+      return controller(req);
+    } catch (error) {
+      if (error instanceof NotFoundError)
+        throw new ServerError(StatusCodes.NOT_FOUND, error.message);
+      throw error;
+    }
+  };
 
-const deleteTask: Controller = (req) => {};
+const createTask = throwIfNotFound((req) => ({
+  code: StatusCodes.CREATED,
+  value: TaskService.createTask(req.body),
+}));
 
-const updateTask: Controller = (req) => {};
+const deleteTask: Controller = throwIfNotFound((req) => ({
+  code: StatusCodes.OK,
+  value: TaskService.deleteTask(Number(req.params.id)),
+}));
+
+const updateTask: Controller = throwIfNotFound((req) => ({
+  code: StatusCodes.OK,
+  value: TaskService.modifiyTask(req.body),
+}));
 
 export default {
-  getAllTasks,
-  createTask,
-  deleteTask,
-  updateTask,
+  getAllTasks: controllerWrapper(getAllTasks),
+  createTask: controllerWrapper(createTask),
+  deleteTask: controllerWrapper(deleteTask),
+  updateTask: controllerWrapper(updateTask),
 };
